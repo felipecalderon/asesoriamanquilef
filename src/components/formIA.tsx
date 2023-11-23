29ea72fee25ebb7f725a6d7e06400e71140ae0f4
Felipe <felipe.calderon321@gmail.com>
2023-11-22 22:25:24 -0300

    ajustes input chatbot

--- src/components/formIA.tsx
+++ src/components/formIA.tsx
@@ -37,17 +37,18 @@ const FormIA = () => {
                     value={query}
                     type="text" 
                     id="consulta"
+                    autoComplete="off"
                     placeholder="Ej. ¿Puedo dejarle testamento a mis mascotas?" 
                     className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" />
             </div>
             {loading && <div className="inline-flex gap-4 items-center">
-                <VscLoading className='text-4xl animate-spin' />
-                <p className="text-lg text-left font-semibold italic">Cargando...</p>
+                <VscLoading className='text-4xl animate-spin dark:text-white' />
+                <p className="text-lg text-left font-semibold italic dark:text-white">Cargando...</p>
             </div>}
         </form>
     </div>
     { respIA && 
-    <p className="px-6 md:px-20 text-lg w-full text-center font-medium italic">{ respIA }</p>
+    <p className="px-6 md:px-20 text-lg w-full text-center font-medium italic dark:text-white">{ respIA }</p>
     }
     </>
     )
