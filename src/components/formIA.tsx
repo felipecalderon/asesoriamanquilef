bef4fca0879b0ced1ba0d14173216c9226b6a992
Felipe <felipe.calderon321@gmail.com>
2023-12-13 20:45:09 -0300

    consulta query ia

--- src/components/formIA.tsx
+++ src/components/formIA.tsx
@@ -5,21 +5,25 @@ import React, { useEffect, useState } from "react"
 import { VscLoading } from "react-icons/vsc";
 import { fetchData } from "@/utils/fetchs";
 import { counterStore } from "@/store/counterStore";
+import Titulo from "./titulo";
+import { LuSendHorizonal } from "react-icons/lu";
+
 const FormIA = () => {
     const [respIA, setResIA] = useState<string | null>(null)
     const [query, setQ] = useState<string>('')
     const [loading, setLoading] = useState<boolean>(false)
     const [animatedText, setAnimatedText] = useState("");
     const { counter, setCounter } = counterStore()
-    
+
     const consultarIA = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault()
         setResIA(null)
-        if(counter < 1) {
+        if(query === '') return setResIA('No escribiste nada, ingresa tu requerimiento')
+        if (counter < 1) {
             setResIA('Se agotaron los intentos, consulte con la abogada Manquilef directamente')
             return setQ('')
         }
-        setCounter(counter-1)
+        setCounter(counter - 1)
         window.localStorage.setItem('counter', (counter - 1).toString())
         e.preventDefault()
         try {
@@ -27,7 +31,7 @@ const FormIA = () => {
             const options: OptionsFetch = {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify({query})
+                body: JSON.stringify({ query })
             };
             const data = await fetchData(options)
             setLoading(false)
@@ -60,25 +64,38 @@ const FormIA = () => {
 
     useEffect(() => {
         const counter = window.localStorage.getItem('counter')
-        if(counter) setCounter(Number(counter))
+        if (counter) setCounter(Number(counter))
         else window.localStorage.setItem('counter', '3')
     }, [])
-    
+
     return (
         <>
             <div className="w-3/4 md:1/3 mx-auto">
-                <form className="space-y-8 py-6" onSubmit={consultarIA}>
-                    <div>
-                        <label htmlFor="consulta" className="block mb-2 text-center text-sm font-medium text-gray-600 dark:text-gray-300">Consulta online sin costo <br />
-                        <span className="italic">(tienes {counter} consultas disponibles)</span></label>
-                        <input
-                            onChange={(e) => setQ(e.target.value)}
-                            value={query}
-                            type="text"
-                            id="consulta"
-                            autoComplete="off"
-                            placeholder="Ej. ¿Puedo dejarle testamento a mis mascotas?"
-                            className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" />
+                <form className="space-y-8 py-16" onSubmit={consultarIA}>
+                    <div className="flex flex-col justify-center items-center">
+                        <div className="text-center py-5 w-fit">
+                            <Titulo message="Consulta gratuita" />
+                        </div>
+                        <label htmlFor="consulta" className="block mb-2 text-center font-bold text-gray-600 dark:text-gray-300">Asistente virtual 24/7<br />
+                            <p className="italic font-extralight">(tienes <span className="font-bold">{counter}</span> consultas disponibles)</p>
+                        </label>
+                        <div className="relative w-full">
+                            <textarea
+                                onChange={(e) => setQ(e.target.value)}
+                                value={query}
+                                id="consulta"
+                                rows={4}
+                                className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                placeholder="Me sucedió lo siguiente... ...Podré dejarle testamento a mis mascotas?"
+                            />
+                            <button 
+                                className="absolute right-3 top-1/2 transform -translate-y-1/2"
+                                type="submit"
+                            >
+                                <LuSendHorizonal className="text-5xl text-violet-500 hover:text-violet-700" />
+                            </button>
+                        </div>
+
                     </div>
                     {loading && <div className="inline-flex gap-4 items-center">
                         <VscLoading className='text-4xl animate-spin dark:text-white' />
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -16,10 +16,10 @@ const Header = () => {
                             Barbara Manquilef
                         </span>
                     </div>
-                    <div className="flex items-center md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+                    <div className="flex items-center md:order-2 space-x-2 md:space-x-0 rtl:space-x-reverse">
                         <button type="button" className="flex text-sm rounded-full md:me-0 focus:ring-2 focus:ring-violet-900 dark:focus:ring-gray-600" id="user-menu-button" aria-expanded="false" data-dropdown-toggle="user-dropdown" data-dropdown-placement="bottom">
                             <span className="sr-only">Abrir perfil</span>
-                            <BiSolidUserCircle className="h-12 w-12 text-violet-900 hover:text-violet-700" />
+                            <BiSolidUserCircle className="text-4xl md:text-5xl text-violet-900 hover:text-violet-700" />
                         </button>
                         <div className="hidden z-50 my-4 text-base list-none bg-violet-100 divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600" id="user-dropdown">
                             <div className="px-4 py-3">
@@ -38,9 +38,9 @@ const Header = () => {
                                 </li>
                             </ul>
                         </div>
-                        <button data-collapse-toggle="navbar-user" type="button" className="inline-flex items-center p-2 w-12 h-12 justify-center text-sm bg-violet-100 text-violet-900 rounded-lg md:hidden hover:bg-violet-50 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-user" aria-expanded="false">
+                        <button data-collapse-toggle="navbar-user" type="button" className="inline-flex items-center p-2 justify-center text-sm bg-violet-100 text-violet-900 rounded-lg md:hidden hover:bg-violet-50 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-user" aria-expanded="false">
                             <span className="sr-only">Abrir menú</span>
-                            <BiMenuAltRight className='text-5xl'/>
+                            <BiMenuAltRight className='text-2xl md:text-5xl'/>
                         </button>
                     </div>
                     <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-user">
