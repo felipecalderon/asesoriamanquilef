62092fa1e1ca79b70dcbc219bd343a0323b76c06
Felipe <felipe.calderon321@gmail.com>
2024-04-28 22:48:51 -0400

    descripcion servicios en home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,6 +17,10 @@ export default function Home() {
       <BannerOpcional />
       <div className="z-10 relative">
         <div className="py-10 px-3">
+          <div className="flex flex-col items-center pb-6">
+            <p className="text-xl font-semibold pb-3 text-primario">Mi compromiso es brindar un servicio legal compasivo, eficiente y estratégico</p>
+            <p className="text-pretty italic max-w-2xl text-center text-slate-700">Ayudándote a enfrentar los desafíos legales con confianza, serenidad y seleridad. <br></br>Con la privacidad y seriedad que cada caso requiere.</p>
+          </div>
           <Servicios />
         </div>
       </div>
