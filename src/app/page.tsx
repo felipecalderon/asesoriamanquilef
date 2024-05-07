fa113b40452c98204474f127df15d531c0ec4999
Felipe <felipe.calderon321@gmail.com>
2024-05-06 23:16:06 -0400

    cambio home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,9 +6,10 @@ import Posts from "@/components/Posts";
 import BannerOpcional from "@/components/bannerv2";
 import { useEffect } from "react";
 import { storePosts } from "@/store/postsStore";
+import { Tooltip } from "@nextui-org/react";
 
 export default function Home() {
-  const {getPosts} = storePosts()
+  const { getPosts } = storePosts()
   useEffect(() => {
     getPosts()
   }, [])
@@ -18,13 +19,21 @@ export default function Home() {
       <div className="z-10 relative">
         <div className="py-10 px-3">
           <div className="flex flex-col items-center pb-6">
-            <p className="text-pretty text-xl font-semibold pb-3 text-center text-primario">Mi compromiso es brindar un servicio legal compasivo, eficiente y estratégico</p>
-            <p className="text-pretty italic max-w-2xl text-center text-slate-700">Ayudándote a enfrentar los desafíos legales con confianza, serenidad y seleridad. <br></br>Con la privacidad y seriedad que cada caso requiere.</p>
+            <p className="text-pretty text-xl max-w-xl font-semibold pb-3 text-center text-primario">Mi compromiso es brindar un servicio jurídico personalizado, eficiente y estratégico, con enfoque&nbsp;
+              <Tooltip
+                showArrow={true}
+                size="md"
+                placement="bottom"
+                color="secondary"
+                content="Protejo los derechos de todos, incluídos los animales no humanos">
+                <span className="font-bold italic hover:text-xl hover:rotate-2 transition-all">antiespecista</span>
+              </Tooltip>.</p>
+            <p className="text-pretty italic max-w-2xl text-center text-slate-700">Te ayudaré a enfrentar los problemas con confianza y celeridad.</p>
           </div>
           <Servicios />
         </div>
       </div>
-        {/* <Posts /> */}
+      {/* <Posts /> */}
       <div className="z-10 relative">
         <FormIA />
         <BGFigura />
